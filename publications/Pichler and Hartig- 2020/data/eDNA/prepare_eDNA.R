3007ad4e12c1685556f5497e669cfa477afde2c9
MaximilianPi <Maximilian.Pichler@ur.de>
2020-07-14 18:41:53 +0200

    correcting wrong paths

--- publications/Pichler and Hartig- 2020/data/eDNA/prepare_eDNA.R
+++ publications/Pichler and Hartig- 2020/data/eDNA/prepare_eDNA.R
@@ -1,23 +1,22 @@
 # prepare eDNA dataset
-# Code was taken and changed from https://github.com/tobiasgf/man_vs_machine/ 
+# Code was taken and modified from https://github.com/tobiasgf/man_vs_machine/ 
 
 all_tabs <- c("ds_survey_full.txt","ds_survey_agaricomycetes.txt","ds_survey_agaricales.txt","ds_soilsurvey_full.txt","ds_soilsurvey_agaricomycetes.txt","ds_soilsurvey_agaricales.txt","ds_dna_full.txt","ds_dna_agaricomycetes.txt","ds_dna_agaricales.txt")
 read_otuk <- c(
-  "data/eDNA/ds_survey_full.txt"              ,
-  "data/eDNA/ds_survey_agaricomycetes.txt"    ,
-  "data/eDNA/ds_survey_agaricales.txt"        ,
-  "data/eDNA/ds_soilsurvey_full.txt"          ,
-  "data/eDNA/ds_soilsurvey_agaricomycetes.txt",
-  "data/eDNA/ds_soilsurvey_agaricales.txt"    ,
-  "data/eDNA/ds_dna_full.txt"                 ,
-  "data/eDNA/ds_dna_agaricomycetes.txt"       ,
-  "data/eDNA/ds_dna_agaricales.txt" 
+  "data/ds_survey_full.txt"              ,
+  "data/ds_survey_agaricomycetes.txt"    ,
+  "data/ds_survey_agaricales.txt"        ,
+  "data/ds_soilsurvey_full.txt"          ,
+  "data/ds_soilsurvey_agaricomycetes.txt",
+  "data/ds_soilsurvey_agaricales.txt"    ,
+  "data/ds_dna_full.txt"                 ,
+  "data/ds_dna_agaricomycetes.txt"       ,
+  "data/ds_dna_agaricales.txt" 
 )
 com_dis_ds <- list() # 
 ttab_ds <- list()
 for(i in 1:length(read_otuk)){
   tab_ds <- read.csv(read_otuk[i],sep='\t',header=T,as.is=TRUE,row.names = 1) #read table
-  tab_ds <- tab_ds[,samples]
   tab_ds <- tab_ds[,-c(29,36,54,103,115)]
   tab_ds[tab_ds>1] <- 1
   ttab_ds[[i]] <- t(tab_ds)
